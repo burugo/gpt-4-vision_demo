305a8cdaa30023660bc38c6b0e833166f55c05a3
tkys <tkysyskw@gmail.com>
2023-11-07 17:02:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ prompt = st.text_input("プロンプトを入力してください:")
 image_file = st.file_uploader("画像をアップロードしてください", type=["jpg", "png", "jpeg"])
 
 if image_file:
-    # アップロードされた画像ファイルをバイナリ形式に変換
+    # アップロード画像->バイナリ形式に変換
     image_data = image_file.read()
     # Base64エンコード
     image_base64 = base64.b64encode(image_data).decode()
@@ -31,10 +31,10 @@ if image_file:
 if st.button("生成") and image_file:
     while True:
         try:
-            # 開始時間を記録
+            # 開始
             start_time = time.time()
 
-            # Chat Completions APIを使用してテキスト生成をリクエスト
+            # テキスト生成リクエスト
             response = openai.chat.completions.create(
                 model="gpt-4-vision-preview",
                 messages=[
@@ -45,25 +45,25 @@ if st.button("生成") and image_file:
                 max_tokens=2000,
             )
 
-            # 終了時間を記録
+            # 終了
             end_time = time.time()
 
-            # 実行時間を表示
+            # 実行時間
             execution_time_ms = (end_time - start_time) * 1000
             st.write(f"実行時間(ms): {execution_time_ms}ms")
 
-            # ChatCompletionオブジェクトから生成されたテキストを取得
+            # 生成されたテキストを取得
             generated_text = response.choices[0].message.content
             st.write("返答:\n" + generated_text)
-            break  # ループを終了してエラーから抜け出す
+            break
 
         except openai.OpenAIError as e:
-            # OpenAI APIからの一般的なエラーをハンドリング
+            # OpenAI APIからの一般的なエラー
             st.write(f"OpenAI APIエラー")
             st.write(e)
-            break  # ループを終了してエラーから抜け出す
+            break
         except Exception as e:
-            # その他の例外をハンドリング
+            # その他のエラー
             st.write(f"エラー")
             st.write(e)
-            break  # ループを終了してエラーから抜け出す
+            break
